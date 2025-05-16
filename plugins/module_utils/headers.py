8496924f711d354f9d8ad2c319ae4fa2ff4741d0
Corey Struzan <corey.struzan@gmail.com>
2025-05-16 12:00:09 -0700

    Modify headers as requested in #2
    Modifying headers as requested in comment on pull request #2

--- plugins/module_utils/headers.py
+++ plugins/module_utils/headers.py
@@ -1,11 +1,8 @@
 from ansible.module_utils.basic import AnsibleModule
 
 BASE_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+    "User-Agent": "ansible-collection-hostinger",
     "Accept": "application/json",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.8",
-    "Referer": "https://hostinger.com",
     "Content-Type": "application/json",
 }
 
